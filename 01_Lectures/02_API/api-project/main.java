8fc830cddc2893d6009707b07dbcf2956f6fb874
Fomin Alexey <fomin.alexey@outlook.com>
2023-12-21 13:46:19 +0300

    Добавил учебные материалы семинара и лекции

---
+++ 01_Lectures/02_API/api-project/main.java
@@ -0,0 +1,19 @@
+public class main {
+    public static void main(String[] args) {
+
+        File f1 = new File("file.txt");
+        File f2 = new File("D:\\Ydisk\\YandexDisk\\Dev_Master\\git\\gb_java_2023\\01_Lectures\\02_API\file.txt");
+
+        
+        
+        /*
+         *  не правильный путь
+         *  отсутсвие папки
+         *  нет файла
+         *  не памяти
+         *  файл поврежденный
+         *  файл был открыт, не доступен
+         */
+
+    }   
+}
