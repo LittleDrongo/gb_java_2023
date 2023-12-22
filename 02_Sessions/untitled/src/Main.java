b619195d5f2ea976aab6c8a4f1bd4237278a4933
Fomin Alexey <fomin.alexey@outlook.com>
2023-12-22 11:33:24 +0300

    hw-1-1 доработал

--- 02_Sessions/untitled/src/Main.java
+++ 02_Sessions/untitled/src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
-    
+
     // method cntNtriangle
-    private static int countNtriangle(int num) {
+    private static int countNtriangle(int n) {
         int res = 0;
-        while (num != 0) {
-            res = res + num;
-            num = num - 1;
+        // Если число n < 1 (ненатуральное) метод должен вернуть -1.
+
+        if (n < 1)
+            return -1;
+
+        while (n != 0) {
+            res = res + n;
+            n = n - 1;
         }
         return res;
     }
 
     public static void main(String[] args) {
-        int tn = countNtriangle(5);
+        int tn = countNtriangle(4);
         System.out.println(tn);
 
     }
